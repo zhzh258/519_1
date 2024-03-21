d66e26e432cc84e1edf9d4590716e9183e135bee
Zhaozhan <nonox530042@gmail.com>
2024-03-21 12:17:12 -0400

    a2

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -7,8 +7,13 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+    // log the kv value to Azure Function App
     const Secret2 = process.env.MyAppSetting;
     context.log(`Value of Secret2 is: ${Secret2}`);
+
+    // store a string in Queue Storage
+    context.bindings.myQueueItem = "abcabcabctestinggggg"
+
 
     context.res = {
         // status: 200, /* Defaults to 200 */
